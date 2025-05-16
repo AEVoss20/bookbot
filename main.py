fab02a288ae4a806998defb28e505ad82082c144
AEVoss20 <aevoss20@gmail.com>
2025-05-15 21:16:13 -0400

    added word counter

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def words(file_contents):
+    num_words = len(file_contents.split())
+    return num_words
+
+def main():
+    from stats import get_book_text
+    book = get_book_text("books/frankenstein.txt")
+    count = words(book)
+    print(f"{count} words found in the document")
+
+main()
