4a3e68d2b17ebcbb1ff563f6ffdf1e653dfc48fe
AEVoss20 <aevoss20@gmail.com>
2025-05-18 18:04:56 -0400

    fixed typo

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     else:
-        book_path = sys.argv(1)
+        book_path = sys.argv[1]
         text = get_book_text(book_path)
         num_words = get_num_words(text)
         chars_dict = get_chars_dict(text)
